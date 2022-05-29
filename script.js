b96c4f958d6a354b9ab1fe5d3cb162d77a10396c
gigi <jaworski.gerard@gmail.com>
2022-05-29 19:57:48 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var buttonFilters = document.getElementById('buttonFilters');
+var divFilters = document.getElementById('divFilters')
+
+buttonFilters.onclick = function(){
+    divFilters.classList.add("active")
+}
